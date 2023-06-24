a627cc9d519b784c1d36ca3d731a20e0d5451bcf
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2023-06-24 11:48:58 +0200

    :iphone: Add Projects Phone  Responsive

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,10 +1,9 @@
 <template>
   <GithubPub />
   <Nav />
-  <div class="flex flex-col w-full lg:gap-[30vh] gap-[30vh]">
-    <section
-      class="w-full lg:h-screen h-fit lg:pt-[9%] pt-[25%] items-center bg-cover bg-top bg-no-repeat bg-fond px-[5%] flex lg:flex-row flex-col justify-between">
-      <div class="flex flex-col lg:w-[40%] w-full gap-8 items-start justify-center">
+  <div class="flex flex-col w-full lg:gap-[30vh] gap-[50vh]">
+    <section class="w-full lg:gap-0 gap-[15vh] lg:h-screen h-fit lg:pt-[9%] pt-[25%] items-center bg-cover bg-top bg-no-repeat bg-fond px-[5%] flex lg:flex-row flex-col justify-between">
+      <div class="flex h-fit flex-col lg:w-[40%] w-full gap-8 items-start justify-center">
         <Citation titre="Mes projets" />
         <h1
           class="lg:text-[80px] text-[50px] font-regular lg:leading-[80px] leading-[60px] border-b-4 pb-4 border-primary">
@@ -22,10 +21,10 @@
       </div>
       <div class="flex flex-col lg:w-1/2 w-full justify-start items-end relative">
         <div class="relative flex w-full hover -top-12">
-          <ProjectCard titre="page d'accueil" desc="Mon" bold="portfolio" url="images/Portfolio-side.jpg"  class="absolute rotate-[24deg] right-24 w-7/12 top-12 z-10"/>
-          <ProjectCard titre="page d'accueil" desc="Site de" bold="fleuriste" url="images/florela-side.jpg" class="w-7/12 h-min z-[1]"/>
+          <ProjectCard titre="page d'accueil" desc="Mon" bold="portfolio" url="images/Portfolio-side.jpg"  class="absolute lg:block hidden rotate-[24deg] right-24 w-7/12 top-12 z-10"/>
+          <ProjectCard titre="page d'accueil" desc="Site de" bold="fleuriste" url="images/florela-side.jpg" class="lg:w-7/12 w-full h-min z-[1]"/>
         </div>
-        <div class="absolute z-10 -bottom-2 -right-8 lg:flex border-primary rounded-full flex text-sm flex-row gap-6 border-solid border-2 px-7 bg-white py-2 w-1/2">
+        <div class="absolute hidden z-10 -bottom-2 -right-8 lg:flex border-primary rounded-full flex text-sm flex-row gap-6 border-solid border-2 px-7 bg-white py-2 w-1/2">
           <img src="images/Window.svg" alt="icone">
           <p>Créer des expériences mémorables et captivantes.</p>
         </div>
@@ -37,6 +36,7 @@
 
 <style scoped >
 
+@media (min-width: 1024px) {
   .-top-12:hover .absolute {
     transform: translateX(10%) rotate(32deg);
     transition: all 0.5s ease-in-out;
@@ -45,7 +45,8 @@
     transform: translateX(-10%) rotate(-12deg);
     transition: all 0.5s ease-in-out;
   }
-
+}
+  
 </style>
 
 <script>
