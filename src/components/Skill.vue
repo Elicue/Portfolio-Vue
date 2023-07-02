77b164b8dd65b70bd53374e0521cc52438322e1c
Lucie Ehrsam <lucie.ehrsam@edu.devinci.fr>
2023-07-02 18:48:47 +0200

    :lipstick: Fix Skills

--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="card border-[1px] lg:w-10/12 border-noir flex flex-col justify-between text-center items-center py-3 px-2">
+  <div class="card border-[1px] lg:w-10/12 lg:h-auto h-[25vh] border-noir flex flex-col justify-between text-center items-center py-3 px-2">
     <div class="titre w-11/12 mx-auto h-1/4 ">
       <h3 class="uppercase font-bold">{{ titre }}</h3>
     </div>
     <div class="content lg:w-11/12 mx-auto h-1/2 justify-center flex">
-      <img class="w-[4vw] h-[4vw] lg:grayscale hover:grayscale-0 hover:scale-105" loading="lazy" :src="url" alt="logo">
+      <img class="lg:w-[4vw] lg:h-[4vw] w-[15vw] h-[15vw] lg:grayscale hover:grayscale-0 hover:scale-105" loading="lazy" :src="url" alt="logo">
     </div>
     <div class="desc lg:w-11/12 mx-auto h-1/4 ">
       <p class=" text-sm">C'est quoi <a  target="_blank" :href="href"><button class="underline underline-offset-4">{{ skill }}</button></a>&nbsp;?</p>    
